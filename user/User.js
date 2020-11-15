2ca13b2b165e0551ec4b46cb5d974c65980ad0f6
Your Name <you@example.com>
2020-11-15 23:44:00 +1100

    update data structure

--- user/User.js
+++ user/User.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({  
   name: String,
   email: String,
-  password: String
-  mobile: String
+  password: String,
+  mobile: String,
   gender: String
 });
 mongoose.model('User', UserSchema);
